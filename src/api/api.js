efefbceddda49227dedeab3d9b4cec79d8e19040
Alonso Sandoval <alonso.sandoval@magnet.cl>
2020-07-15 18:12:34 -0400

    demo version, remove src code

--- src/api/api.js
+++
@@ -1,6 +0,0 @@
-export const apiCall = (link) =>
-  fetch(link).then(response => response.json())
-
-export const capitalizeFirstLetter = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
